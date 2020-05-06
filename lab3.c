4f999a5ba543c791f40571140327ce7e43fe4d20
GSemix <63607553+GSemix@users.noreply.github.com>
2020-05-06 15:50:58 +0300

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -3,6 +3,90 @@
 #include <string.h>
 #include <malloc.h>
 
+int plus_memory (char ** line, int * count, char * some_letter) {
+	*count += strlen(some_letter);
+	*line = realloc(*line, *count);
+
+	return 0;
+}
+
+int line_processing (char * line) {
+	char a;
+	char b;
+	int clone = 1;
+	char * new_line = malloc(1);
+
+	for (int i = 0; i < strlen(line); i++) {
+		a = line[i];
+		b = line[i + 1];
+//printf("#%s$%s#", other_letters[0], other_letters[1]);
+
+		if (a == b && a != ' ' && a != '\t') clone += 1;
+		else {
+
+			if (clone > 1) {
+
+				for (int j = 0; j < clone; j++) {
+					new_line = realloc(new_line, strlen(new_line) + 1);
+					strcat(new_line, &a);
+				}
+
+				new_line = realloc(new_line, strlen(new_line) + 1);
+				strcat(new_line, "1");
+				clone = 1;
+			}
+
+		}
+
+	}
+
+	printf("\" %s\"\n", new_line);
+	free(new_line);
+
+	return 0;
+}
+
+int clear_memory (char ** line, int * count) {
+	free(*line);
+	*count = 1;
+	*line = malloc(*count);
+
+	return 0;
+}
+
+int main () {
+	char some_letter[11];
+	int count = 1;
+	int sc = 1;
+	char * line = malloc(count);
+
+	do {
+		sc = scanf("%10[^\n]", some_letter);
+
+		if (sc == -1) break;
+		else if (sc == 1) {
+			plus_memory(&line, &count, some_letter);
+			strcat(line, some_letter);
+		} else {
+			scanf("%*c");
+			if (strlen(line) != 0) line_processing(line);
+				clear_memory(&line, &count);
+		}
+
+	} while (sc != -1);
+	
+	return 0;
+}	
+
+
+
+
+
+/*#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+
 int plus_memory (char ** line, int  * count, char * some_letter) {
 	*count += strlen(some_letter);
 	*line = realloc(*line, *count);
@@ -70,4 +154,4 @@ int main () {
 	} while (sc != -1);
 	
 	return 0;
-}
+}*/
